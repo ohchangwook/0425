fbadc0c0b4033cd00e47ebee2d896704acdb3444
ohchangwook <63888093+ohchangwook@users.noreply.github.com>
2020-04-25 12:21:42 +0900

    Add files via upload

---
+++ 0425/basic 02.py
@@ -0,0 +1,20 @@
+import turtle as t
+t.shape("turtle")
+
+t.write(t.position())
+
+t.forward(100)
+
+t.write(t.position())
+
+t.left(120)
+t.forward(100)
+
+t.write(t.position())
+
+t.left(120)
+
+t.forward(100)
+
+t.write(t.position())
+
